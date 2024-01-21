3f853586a284c36506d5b220bb1e324e54742563
brianach <brianocualain@outlook.ie>
2024-01-21 12:21:03 +0000

    fix confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from store.models import StoreItem
+from profiles.models import UserProfile
 
+import stripe
 import json
 import time
 
@@ -54,9 +59,9 @@ class StripeWH_Handler:
             intent.latest_charge
         )
 
-        billing_details = intent.charges.data[0].billing_details
+        billing_details = stripe_charge.billing_details # updated
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2) # updated
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
