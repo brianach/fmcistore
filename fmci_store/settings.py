90458fd6a15e6c2c72526febb0d622c50630f9af
brianach <brianocualain@outlook.ie>
2024-01-29 21:08:46 +0000

    fix smpt email handler

--- fmci_store/settings.py
+++ fmci_store/settings.py
@@ -220,8 +220,8 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAILR = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 DEFAULT_FROM_EMAIL = 'store@fmci.ie'
