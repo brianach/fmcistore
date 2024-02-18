3a8e3d61216bca8f33f595044b52d007c3aa99de
brianach <brianocualain@outlook.ie>
2024-02-18 16:43:09 +0000

    add partner content and modify carousel styling

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import PartnerImages
 
 # Register your models here.
+class PartnerImagesAdmin(admin.ModelAdmin):
+    list_display = (
+        'p_name',
+        'p_link',
+    )
+
+
+admin.site.register(PartnerImages, PartnerImagesAdmin)
