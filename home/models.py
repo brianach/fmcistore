e0cfe92a854f93c7654a516c734783e05328c2fd
brianach <brianocualain@outlook.ie>
2024-02-18 15:32:58 +0000

    add partner carousel to end of home page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class PartnerImages(models.Model):
+
+    p_name = models.CharField(max_length=254)
+    p_image = models.ImageField(null=True, blank=True)
+    p_link = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.p_name
--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import PartnerImages
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+
+    p_images = PartnerImages.objects.all()
+
+    return render(request, 'home/index.html', {
+        'partner_images': p_images,
+    })
